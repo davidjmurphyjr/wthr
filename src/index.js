3d6905e696cc94be19bf134078cf8b68dc7d114a
davidjmurphyjr <davidjmurphyjr@gmail.com>
2018-06-26 08:28:44 -0400

    add high charts, log weather data

--- src/index.js
+++ src/index.js
@@ -1,24 +1,54 @@
 import join from 'lodash/join';
+import xml2js from 'xml2js';
+import Highcharts from 'highcharts';
 
-function component() {
-    var element = document.createElement('div');
-    element.innerHTML = join(['webpack', 'loaded'], ' ');
-    return element;
-}
+var xhr = new XMLHttpRequest();
 
-document.body.appendChild(component());
+console.log('UNSENT', xhr.readyState); // readyState will be 0
 
-var httpRequest = new XMLHttpRequest();
-httpRequest.onreadystatechange = function() {
-    if (httpRequest.readyState === XMLHttpRequest.DONE) {
-      var element = document.createElement('div');
-      element.innerHTML = "http request succeeded"
-      document.body.appendChild(element);
-    } else {
-      var element = document.createElement('div');
-      element.innerHTML = this.readyState;
-      document.body.appendChild(element);
-    }
+xhr.open("GET", "https://forecast.weather.gov/MapClick.php?lat=42.3944&lon=-71.1165&FcstType=digitalDWML", true);
+console.log('OPENED', xhr.readyState); // readyState will be 1
+
+xhr.onprogress = function () {
+    console.log('LOADING', xhr.readyState); // readyState will be 3
 };
-httpRequest.open("GET", "https://forecast.weather.gov/MapClick.php?lat=42.3944&lon=-71.1165&FcstType=digitalDWML", true);
-httpRequest.send();
+
+xhr.onload = function () {
+    console.log('DONE', xhr.readyState); // readyState will be 4
+    var element = document.createElement('div');
+    document.body.appendChild(element);
+
+    xml2js.parseString(xhr.response, function (err, result) {
+      element = document.createElement('div');
+      console.log(result);
+      makeGraph(element);
+    });
+};
+    
+xhr.send();
+
+function makeGraph(element) {
+  Highcharts.chart('container', {
+    chart: {
+      type: 'bar'
+    },
+    title: {
+      text: 'Fruit Consumption'
+    },
+    xAxis: {
+      categories: ['Apples', 'Bananas', 'Oranges']
+    },
+    yAxis: {
+      title: {
+        text: 'Fruit eaten'
+      }
+    },
+    series: [{
+      name: 'Jane',
+      data: [1, 0, 4]
+    }, {
+      name: 'John',
+      data: [5, 7, 3]
+    }]
+  });
+}
